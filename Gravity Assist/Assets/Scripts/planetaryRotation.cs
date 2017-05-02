1039475b919cff03398eb78271b07dd96767ef5a
Danielos15 <danielos15@ru.is>
2017-05-02 14:19:18 +0000

    adding all to project test

---
+++ Gravity Assist/Assets/Scripts/planetaryRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planetaryRotation : MonoBehaviour {
+	private Transform trans;
+
+	// Use this for initialization
+	void Start () {
+		trans = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		trans.Rotate (trans.forward * 10 * Time.deltaTime);
+		trans.Rotate (trans.up * 20 * Time.deltaTime);
+	}
+}
