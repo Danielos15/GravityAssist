e800728a1d26c36732e9485b46f8cdd2e4e48033
Danielos15 <danielos15@ru.is>
2017-05-03 11:23:24 +0000

    updates and commit before ignore changes

--- Gravity Assist/Assets/Scripts/PlayerCollision.cs
+++ Gravity Assist/Assets/Scripts/PlayerCollision.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour {
 
+	GameController gameManager;
+	private Transform trans;
+	private Rigidbody2D rigid;
+
 	// Use this for initialization
 	void Start () {
-		
+		trans = GetComponent<Transform> ();
+		rigid = GetComponent<Rigidbody2D> ();
+		gameManager = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Astroid" || other.gameObject.tag == "CelestialObject") {
 			Destroy (gameObject);
+			//TODO:
+			// Explosion, scorescreen
+		} else if (other.gameObject.tag == "Finish") {
+			gameManager.endGame ();
+		}
+	}
+	void OnTriggerStay2D(Collider2D other) {
+		if (other.gameObject.tag == "Finish") {
+			rigid.velocity = Vector2.zero;
+			trans.position = Vector2.MoveTowards (trans.position, other.GetComponent<Transform> ().position, 5.0f* Time.deltaTime);
 		}
 	}
 }
