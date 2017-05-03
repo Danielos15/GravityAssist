f776ae2cd7afb117fc24c77c1ec22fecff0c48d5
Danielos15 <danielos15@ru.is>
2017-05-03 13:54:36 +0000

    fixes in the gravity

--- Gravity Assist/Assets/Scripts/Gravity.cs
+++ Gravity Assist/Assets/Scripts/Gravity.cs
@@ -8,24 +8,22 @@ public class Gravity : MonoBehaviour {
 	private Transform trans;
 	private Rigidbody2D rigid;
 	private GameController gameManager;
-	private AsteroidMovement asteroidStarted;
 
 	// Use this for initialization
 	void Start () {		
 		trans = GetComponent<Transform> ();
 		rigid = GetComponent<Rigidbody2D> ();
 		gameManager = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
-		asteroidStarted = GameObject.FindGameObjectWithTag ("Astroids").GetComponent<AsteroidMovement> ();
 	}
 
 	void FixedUpdate() {
-		if (gameManager.isStarted()) {
-			Collider2D[] colliders = Physics2D.OverlapCircleAll (trans.position, range);
-			List<Rigidbody2D> rbs = new List<Rigidbody2D> ();
+		Collider2D[] colliders = Physics2D.OverlapCircleAll (trans.position, range);
+		List<Rigidbody2D> rbs = new List<Rigidbody2D> ();
 
-			foreach (Collider2D col in colliders) {
-				Rigidbody2D rb = col.attachedRigidbody;
-				if (rb != null && rb != this.rigid && !rbs.Contains (rb)) {
+		foreach (Collider2D col in colliders) {
+			Rigidbody2D rb = col.attachedRigidbody;
+			if (rb != null && rb != this.rigid && !rbs.Contains (rb)) {
+				if (col.gameObject.tag == "Player" && gameManager.isStarted ()) {
 					rbs.Add (rb);
 					Vector2 offset = trans.position - col.transform.position;
 					rb.AddForce (offset / offset.sqrMagnitude * this.rigid.mass);
