38136fcf46b8e0e568a8ec07b49fc530cbf72899
eyrunm <eyrun15@ru.is>
2017-05-03 12:01:17 +0000

    main menu scene

---
+++ Gravity Assist/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState{
+	Menu,	//default menu state
+	NewGame,	//Load objects into game
+	Start		//Playing the game
+}
+
+public class GameManager : MonoBehaviour {
+
+	public GameState gameState;
+
+
+	// Use this for initialization
+	void Start () {
+		gameState = GameState.Menu;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameState = GameState.NewGame) {
+			//Instantiate the player into the game
+			//(Set camera to his position)
+		}
+
+
+	}
+}
