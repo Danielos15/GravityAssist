797efd73f4b201a8bfb8db131a296b7ac576e154
Danielos15 <danielos15@ru.is>
2017-05-10 16:59:10 +0000

    added circle around planets

---
+++ Gravity Assist/Assets/ColorTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorTest : MonoBehaviour {
+
+	private Color color;
+	private MeshRenderer mr;
+	public Color test;
+
+	// Use this for initialization
+	void Start () {
+		Gravity planetGravity = GetComponentInParent<Gravity> ();
+		float planetMass = GetComponentInParent<Rigidbody2D> ().mass;
+		Debug.Log (planetMass);
+		color = new Color (0.5490196f, 0.627451f, 0.01960784f, planetGravity.range * 0.02f);
+		Debug.Log ("Red " + test.r);
+		Debug.Log ("Green " + test.g);
+		Debug.Log ("Blue " + test.b);
+		Debug.Log ("Alpha " + test.a);
+		mr = GetComponent<MeshRenderer> ();
+	
+	}
+
+	void Update() {
+		mr.material.SetColor ("_TintColor", color);
+	}
+}
