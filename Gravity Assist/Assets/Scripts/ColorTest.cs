aeda1987eb2e693a36001e2c1ffac0461048c33d
Danielos15 <danielos15@ru.is>
2017-05-10 18:51:08 +0000

    starting on distance indicator

--- Gravity Assist/Assets/Scripts/ColorTest.cs
+++ Gravity Assist/Assets/Scripts/ColorTest.cs
@@ -12,14 +12,8 @@ public class ColorTest : MonoBehaviour {
 	void Start () {
 		Gravity planetGravity = GetComponentInParent<Gravity> ();
 		float planetMass = GetComponentInParent<Rigidbody2D> ().mass;
-		Debug.Log (planetMass);
 		color = new Color (0.5490196f, 0.627451f, 0.01960784f, planetGravity.range * 0.02f);
-		Debug.Log ("Red " + test.r);
-		Debug.Log ("Green " + test.g);
-		Debug.Log ("Blue " + test.b);
-		Debug.Log ("Alpha " + test.a);
 		mr = GetComponent<MeshRenderer> ();
-	
 	}
 
 	void Update() {
