2e93d7296aee0435b206b01f20958cece3429f5e
Danielos15 <danielos15@ru.is>
2017-05-11 15:54:56 +0000

    starting on gameOptions

---
+++ Gravity Assist/Assets/GameOptions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOptions : MonoBehaviour {
+	private static GameOptions _instance;
+	private static bool soundOn;
+
+	void Awake() {
+		if (!_instance) {
+			soundOn = true;
+			_instance = this;
+			DontDestroyOnLoad (gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	public static GameOptions GetInstance() {
+		return _instance;
+	}
+
+
+	public bool isSoundOn() {
+		return soundOn;
+	}
+
+}
