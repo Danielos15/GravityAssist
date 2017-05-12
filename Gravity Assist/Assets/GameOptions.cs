22c81e6ad28fa07a375948cbb639c9be1413f73f
Danielos15 <danielos15@ru.is>
2017-05-12 13:19:47 +0000

    update to GameOptions

--- Gravity Assist/Assets/GameOptions.cs
+++ Gravity Assist/Assets/GameOptions.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class GameOptions : MonoBehaviour {
 	private static GameOptions _instance;
-	private static bool soundOn;
+
+	private string colPlayerName = "playerName";
+	private string colMusic = "music";
+	private string colSoundEffects = "sound";
 
 	void Awake() {
 		if (!_instance) {
-			soundOn = true;
 			_instance = this;
 			DontDestroyOnLoad (gameObject);
 		} else {
@@ -16,13 +18,99 @@ public class GameOptions : MonoBehaviour {
 		}
 	}
 
-	public static GameOptions GetInstance() {
+	public static GameOptions getInstance() {
 		return _instance;
 	}
 
 
-	public bool isSoundOn() {
-		return soundOn;
+	// GETTERS
+	public string getName() {
+		return getString (colPlayerName);
 	}
 
+	public bool getMusicSettings() {
+		return getBool (colMusic);
+	}
+
+	public bool geSoundSettings() {
+		return getBool (colSoundEffects);
+	}
+
+	public int getBestScoreForLevel(string levelName) {
+		return getInt (levelName);
+	}
+
+
+	// SETTERS
+	public void setName(string name) {
+		saveString (colPlayerName, name);
+	}
+
+	public void setMusicSettings(bool b) {
+		saveBool (colMusic, b);
+	}
+
+	public void setSoundSettings(bool b) {
+		saveBool (colSoundEffects, b);
+	}
+
+	public void setBestScoreForLevel(string levelName, int score) {
+		if (isScoreBetter(levelName, score)) {
+			saveInt (levelName, score);
+		}
+	}
+
+
+	// CHECKERS
+	public bool isNameSet() {
+		return PlayerPrefs.HasKey (colPlayerName);
+	}
+
+	public bool isScoreBetter(string levelName, int score) {
+		int currentBest = getInt (levelName);
+		if (score > currentBest) {
+			return true;
+		}
+		return false;
+	}
+
+	public bool isLevelDone(string levelName) {
+		return PlayerPrefs.HasKey (levelName);
+	}
+
+
+	// Get from PlayerPrefs
+	private string getString(string key) {
+		return PlayerPrefs.GetString (key, "");
+	}
+
+	private int getInt(string key) {
+		return PlayerPrefs.GetInt (key, 0);
+	}
+
+	private bool getBool(string key) {
+		return (PlayerPrefs.GetInt (key, 0) != 0);
+	}
+
+
+	// Save to PlayerPrefs
+	private void saveString(string key, string s) {
+		PlayerPrefs.SetString (key, s);
+		save ();
+	}
+
+	private void saveInt(string key, int i) {
+		PlayerPrefs.SetInt (key, i);
+		save ();
+	}
+
+	private void saveBool(string key, bool b) {
+		PlayerPrefs.SetInt (key, (b ? 1 : 0));
+		save ();
+	}
+
+	//Save to file
+	private void save() {
+		PlayerPrefs.Save ();
+	}
 }
