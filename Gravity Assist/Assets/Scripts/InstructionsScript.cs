2a1ca2479c9d52fb177d931857dac4e35f1ed61e
eyrunm <eyrun15@ru.is>
2017-05-13 14:58:56 +0000

    Instructions on level 1

---
+++ Gravity Assist/Assets/Scripts/InstructionsScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionsScript : MonoBehaviour {
+
+	public GameObject instructions;
+	public Color start;
+	Color end;
+	float duration = 6;
+	float t = 0.005f;
+
+	// Use this for initialization
+	void Start () {
+		end.r = 255;
+		end.g = 0;
+		end.b = 0;
+		end.a = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ChangeColor ();
+	}
+
+	void ChangeColor() {
+		instructions.GetComponent<TextMesh> ().color = Color.Lerp (start, end, t);
+		Debug.Log (t);
+		if (t < 1) {
+			t += Time.deltaTime / duration;
+		}
+
+	}
+}
---
+++ Gravity Assist/Assets/Scripts/imageChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class imageChange : MonoBehaviour {
+	public GameObject image;
+	float duration = 6;
+	float t = 0.005f;
+	Color end;
+	public Color start;
+
+	// Use this for initialization
+	void Start () {
+		end.r = 255;
+		end.g = 0;
+		end.b = 0;
+		end.a = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		image.GetComponent<SpriteRenderer>().color= Color.Lerp (start, end, t);
+			if (t < 1) {
+				t += Time.deltaTime / duration;
+			}
+		}
+}
