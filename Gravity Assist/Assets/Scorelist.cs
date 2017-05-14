94c1ed8615f45418597dbe73ae3ab2bf55a64e9f
Danielos15 <danielos15@ru.is>
2017-05-14 14:20:10 +0000

    added scores to the leaderboards

--- Gravity Assist/Assets/Scorelist.cs
+++ Gravity Assist/Assets/Scorelist.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scorelist : MonoBehaviour {
 
@@ -8,14 +9,21 @@ public class Scorelist : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 10; i++) {
+		// TODO Add loading symbol
+	}
+
+	public int setScore(Score[] scores) {
+		int pos = 1;
+		foreach(Score score in scores) {
+			pos += 1;
 			GameObject test = Instantiate (scorePrefab);
+			test.transform.FindChild ("Place").GetComponent<Text> ().text = pos.ToString();
+			test.transform.FindChild ("Username").GetComponent<Text> ().text = score.user_name;
+			test.transform.FindChild ("Score").GetComponent<Text> ().text = score.score.ToString();
+
 			test.transform.SetParent (this.transform);
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		// Disable the Loading
+		return pos;
 	}
 }
